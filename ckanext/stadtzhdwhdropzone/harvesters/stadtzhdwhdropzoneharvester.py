d9cbe0157031e79cce6df091ea4e50800040d395
waldvogel <waldvogel@liip.ch>
2014-10-23 10:28:29 +0200

    change path variable, code style fixes

--- ckanext/stadtzhdwhdropzone/harvesters/stadtzhdwhdropzoneharvester.py
+++ ckanext/stadtzhdwhdropzone/harvesters/stadtzhdwhdropzoneharvester.py
@@ -1,6 +1,4 @@
-#coding: utf-8
-
-import os
+# coding: utf-8
 
 from pylons import config
 from ckan.model import Session
@@ -9,12 +7,13 @@ from ckanext.stadtzhharvest.harvester import StadtzhHarvester
 import logging
 log = logging.getLogger(__name__)
 
+
 class StadtzhdwhdropzoneHarvester(StadtzhHarvester):
     '''
     The harvester for the Stadt ZH DWH Dropzone
     '''
 
-    DROPZONE_PATH = '/usr/lib/ckan/DWH'
+    DATA_PATH = '/usr/lib/ckan/DWH'
     METADATA_PATH = config.get('metadata.metadatapath', '/usr/lib/ckan/diffs/dwh-metadata')
 
     def info(self):
@@ -28,16 +27,13 @@ class StadtzhdwhdropzoneHarvester(StadtzhHarvester):
             'form_config_interface': 'Text'
         }
 
-
     def gather_stage(self, harvest_job):
         log.debug('In StadtzhdwhdropzoneHarvester gather_stage')
         return self._gather_datasets(harvest_job)
 
-
     def fetch_stage(self, harvest_object):
         log.debug('In StadtzhdwhdropzoneHarvester fetch_stage')
         return self._fetch_datasets(harvest_object)
-
 
     def import_stage(self, harvest_object):
         log.debug('In StadtzhdwhdropzoneHarvester import_stage')
